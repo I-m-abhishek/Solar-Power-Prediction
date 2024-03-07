20925eea58070ae4a46601ccfd226538adb82118
Abhishek katiyar <akatiyar0987@gmail.com>
2024-03-07 23:23:48 +0530

    Fully completed

--- solar_power_prediction/src/app/api/mongodbconnect/route.js
+++ solar_power_prediction/src/app/api/mongodbconnect/route.js
@@ -1,9 +1,8 @@
 import { MongoClient } from 'mongodb';
-const pass= "TvcP9Lkmqr2XzOhw";
-const uri = `mongodb+srv://abhishekkatiyar2504:${pass}@cluster0.cpjc4uq.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.Db_user}:${process.env.Db_pass}@cluster0.vs6u1vr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const uri_new="mongodb://localhost:27017";
 
-const client = new MongoClient(uri_new);
+const client = new MongoClient(uri);
 
 export async function connectToDatabase() {
   if (client && client.topology && client.topology.isConnected()) await client.connect();
